9e9d1ca855eafe198006f728c9a318e5dc1d5128
88Nataliia88 <88karamelka88@mail.ru>
2026-06-25 20:48:35 +0300

    created brain_gcd file

--- brain_games/games/brain_calc.py
+++ brain_games/games/brain_calc.py
@@ -6,7 +6,7 @@ def creating_game_logic():
     random_number1 = random.randint(1, 100)
     random_number2 = random.randint(1, 100)
     operator = random.choice(['+', '-', '*'])
-    qestion = f"{random_number1}{operator}{random_number2}"
+    question = f"{random_number1}{operator}{random_number2}"
 
     match operator:
             case '+':
@@ -16,4 +16,4 @@ def creating_game_logic():
             case '*':
                 correct_answer = random_number1 * random_number2
         
-    return qestion, correct_answer
+    return question, correct_answer
---
+++ brain_games/games/brain_gcd.py
@@ -0,0 +1,18 @@
+import random
+
+DESCRIPTION = ('Find the greatest common divisor of given numbers.')
+
+def creating_game_logic():
+    random_number1 = random.randint(1, 100)
+    random_number2 = random.randint(1, 100)
+    question = f"{random_number1} {random_number2}"
+
+    while random_number2 != 0:
+        random_number1 = random_number2
+        random_number1 ,random_number2 = random_number2, random_number1 % random_number2
+
+    correct_answer = str(random_number1)
+
+    
+
+    return question, correct_answer
